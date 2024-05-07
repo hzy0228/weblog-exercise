985c07b421e193b02e3ca5a2cc0bf3fc6d49f4d7
hao.zhaoyu <hzy_0228@163.com>
2024-05-08 00:07:54 +0800

    项目搭建

--- weblog-web/src/main/java/top/hdy/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/top/hdy/weblog/web/controller/TestController.java
@@ -1,11 +1,17 @@
 package top.hdy.weblog.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import top.hdy.weblog.module.common.aspect.ApiOperationLog;
+import top.hdy.weblog.module.common.utils.Response;
 import top.hdy.weblog.web.model.User;
+
+import java.util.stream.Collectors;
 
 /**
  * @Author: Hao-ZY
@@ -16,8 +22,14 @@ import top.hdy.weblog.web.model.User;
 public class TestController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        // 返参
-        return user;
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorsMsg = bindingResult.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+            return Response.fail(errorsMsg);
+        }
+        return Response.success("参数没有问题");
     }
+
 }
