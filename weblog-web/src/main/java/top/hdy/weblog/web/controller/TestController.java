c6c691d33ef39ccf1c63be7c960adf2a95baca55
hao.zhaoyu <hzy_0228@163.com>
2024-05-08 23:03:25 +0800

    后端基础项目搭建完成

--- weblog-web/src/main/java/top/hdy/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/top/hdy/weblog/web/controller/TestController.java
@@ -1,19 +1,20 @@
 package top.hdy.weblog.web.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import top.hdy.weblog.module.common.aspect.ApiOperationLog;
-import top.hdy.weblog.module.common.enums.ResponseCodeEnum;
-import top.hdy.weblog.module.common.exception.BizException;
+import top.hdy.weblog.module.common.utils.JsonUtil;
 import top.hdy.weblog.module.common.utils.Response;
 import top.hdy.weblog.web.model.User;
 
-import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * @Author: Hao-ZY
@@ -21,24 +22,21 @@ import java.util.stream.Collectors;
  **/
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
-//        // 是否存在校验错误
-//        if (bindingResult.hasErrors()) {
-//            // 获取校验不通过字段的提示信息
-//            String errorsMsg = bindingResult.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
-//            return Response.fail(errorsMsg);
-//        }
-//        return Response.success("参数没有问题");
+    @ApiOperation("测试接口")
+    public Response test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
 
-//        // 手动抛异常，入参是前面定义好的异常码枚举，返参统一交给全局异常处理器搞定
-//        throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND);
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
 
-        // 主动定义一个运行时异常，分母不能为零
-        int i = 1 / 0;
-        return Response.success();
+        return Response.success(user);
     }
 
 }
--- weblog-web/src/main/java/top/hdy/weblog/web/model/User.java
+++ weblog-web/src/main/java/top/hdy/weblog/web/model/User.java
@@ -1,14 +1,19 @@
 package top.hdy.weblog.web.model;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * @Author: Hao-ZY
  * @Date: 2024/4/30 17:46
  **/
 @Data
+@ApiModel(value = "用户实体类")
 public class User {
     // 用户名
     @NotBlank(message = "用户名不能为空") // 注解确保用户名不为空
@@ -27,4 +32,11 @@ public class User {
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")  // 注解确保邮箱格式正确
     private String email;
+
+    // 创建时间
+    private LocalDateTime createTime;
+    // 更新日期
+    private LocalDate updateDate;
+    // 时间
+    private LocalTime time;
 }
